38a41a90d2d276a5d566eb9d6679326d7e185ff4
Sue Grimes <sgrimes@stanford.edu>
2011-04-12 23:51:03 -0700

    Additional formatting changes for forms Rename toggle_div helper to div_toggle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  def toggle_div(div, div1=nil)
+  def div_toggle(div, div1=nil)
     update_page do |page|
       page[div].toggle
       page[div1].toggle if div1
     end
+  end
+  
+  def break_clear
+    '<br /><table width=100%><tr><td></td></tr></table>'
   end
   
   def remove_this_line(div)
