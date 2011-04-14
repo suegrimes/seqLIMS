58eb1a301a9b8dd05d60dcf27072e033d53c1773
Sue Grimes <sgrimes@stanford.edu>
2011-04-14 16:04:22 -0700

    Changes to attached_files/seq_libs forms and info boxes

--- app/helpers/seq_libs_helper.rb
+++ app/helpers/seq_libs_helper.rb
@@ -3,4 +3,9 @@ module SeqLibsHelper
     adapters.reject! {|adapter| adapter.c_value[0,1] == plex_char}
     return adapters
   end
+  
+  def sample_conc_string(sample_conc, uom)
+    conc = (sample_conc ? number_with_precision(sample_conc, :precision => 2) : '--')
+    return [conc, uom].join(' ')
+  end
 end
