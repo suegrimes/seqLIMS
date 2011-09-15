5bfc543802408b480cd980bdd00e63182acb206f
Sue Grimes <sgrimes@stanford.edu>
2011-09-15 10:22:45 -0700

    Fix assigned_barcodes validation - Only check for overlapping range/existing samples when creating new range

--- app/models/assigned_barcode.rb
+++ app/models/assigned_barcode.rb
@@ -17,14 +17,19 @@ class AssignedBarcode < ActiveRecord::Base
   validates_numericality_of :start_barcode, :end_barcode, :only_integer => true
   validates_date :assign_date
 
+  validate_on_create :check_range
+  
   def self.count_overlapping_range(rstart, rend)
     return self.count(:conditions => ["start_barcode <= ? AND end_barcode >= ?", rend, rstart])
   end
   
 protected
   def validate
-    errors.add_to_base("Overlaps existing assigned range(s)") if AssignedBarcode.count_overlapping_range(start_barcode, end_barcode) > 0
-    errors.add_to_base("Existing samples in LIMS in this range") if Sample.count_samples_in_range(start_barcode, end_barcode) > 0
     errors.add(:end_barcode, "- cannot be less than start barcode") if end_barcode < start_barcode 
+  end
+
+  def check_range
+    errors.add_to_base("Overlaps existing assigned range(s)") if AssignedBarcode.count_overlapping_range(start_barcode, end_barcode) > 0
+    errors.add_to_base("Existing samples in LIMS in this range") if Sample.count_samples_in_range(start_barcode, end_barcode) > 0   
   end
 end
