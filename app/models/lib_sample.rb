1f6145db8c496753d21c3f6a6a7102d978ff59e1
Sue Grimes <sgrimes@stanford.edu>
2011-10-19 15:22:04 -0700

    Work in progress (multiplex library redesign)

--- app/models/lib_sample.rb
+++ app/models/lib_sample.rb
@@ -21,7 +21,7 @@
 class LibSample < ActiveRecord::Base
   
   belongs_to :seq_lib
-  belongs_to :mplex_lib, :class_name => 'SeqLib', :foreign_key => :mplex_lib_id
+  belongs_to :splex_lib, :class_name => 'SeqLib', :foreign_key => :splex_lib_id
   belongs_to :processed_sample
   
   validates_presence_of :sample_name
@@ -36,4 +36,27 @@ class LibSample < ActiveRecord::Base
     self.processed_sample = ProcessedSample.find(:first, :conditions => ["barcode_key = ?", barcode]) if !barcode.blank?
   end
   
+  def singleplex_lib
+    return splex_lib_barcode
+  end
+  
+  def singleplex_lib=(lib_barcode)
+    self.splex_lib_barcode = lib_barcode
+    self.splex_lib = nil if lib_barcode.blank?
+    
+    if !lib_barcode.blank?
+      slib = SeqLib.find(:first, :include => :lib_samples, :conditions => ["barcode_key = ?", lib_barcode]) 
+      if slib && slib.lib_samples
+        ssample = slib.lib_samples[0]
+        self.splex_lib = slib
+        self.splex_lib_barcode = slib.barcode_key
+        self.processed_sample_id = ssample.processed_sample_id
+        self.sample_name = ssample.sample_name
+        self.source_DNA  = ssample.source_DNA
+        self.runtype_adapter = ssample.runtype_adapter
+        self.index_tag = ssample.index_tag
+        self.enzyme_code = ssample.enzyme_code
+      end
+    end
+  end
 end
