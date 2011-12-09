8bcdbfd82d37d3dc980c8fd09d75d032b62f7c66
Sue Grimes <sgrimes@stanford.edu>
2011-12-09 14:26:53 -0800

    Modify role/authorization for DEMO site

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ActiveRecord::Base
   
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :password, :password_confirmation
+  attr_accessible :login, :email, :password, :password_confirmation, :roles
 
   # has_role? simply needs to return true or false whether a user has a role or not.  
   # It may be a good idea to have "admin" roles return true always
@@ -62,10 +62,8 @@ class User < ActiveRecord::Base
   # Clin_admin can additionally see all users with role = clinical, or clin_admin
   # Admin can see all users
   def self.find_all_with_authorization(user=current_user)
-    if user.has_role?("admin")
+    if user.has_role?("admin") && !DEMO_APP
       condition_array = []
-    elsif user.has_role?("clin_admin")
-      condition_array = ["roles.name IN (?)", ["clin_admin", "clinical"]]
     else
       condition_array = ["users.login = ?", user.login]
     end
