e81fde013e30b57ece94095c9e999b556b094d59
jpalm <jennifer.palm13@gmail.com>
2012-01-12 17:49:57 -0800

    added del run dir form date autofill

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,3 +13,29 @@ function remove_fields(link) {
 $(link).previous("input[type=hidden]").value = "1";  
 $(link).up(".fields").hide();  
 }
+
+function num_date() {
+    var mm = new Array("01", "02", "03","04", "05", "06", "07", "08", "09","10", "11", "12");
+    var full_date = new Date();
+    var curr_date = full_date.getDate();
+    var curr_month = full_date.getMonth();
+    var curr_year = full_date.getFullYear();
+    var num_date = curr_year + "-" + mm[curr_month] + "-" + curr_date;
+    return num_date;
+}
+
+var $j = jQuery.noConflict(); //use jQuery object with $j and won't conflict with other libraries
+
+var load_del_date = function(id) {
+    var delete_flag_id = "run_dir_" + id + "_delete_flag";
+    var date_deleted_id = "run_dir_" + id + "_date_deleted";
+    //alert(chk_bx_id + " " + date_deleted_id);
+	$j('#' + delete_flag_id).click(function() {
+	    if ( $j('#' + delete_flag_id).is(':checked') ) { 
+	        $j('#' + date_deleted_id).val(num_date());
+        } else { 
+	        $j('#' + date_deleted_id).val(''); 
+        }
+	});
+};
+
