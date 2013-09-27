bf50a93eb0d1ba884de7c9d62e7432480e50f9ff
Sue Grimes <sgrimes@stanford.edu>
2013-09-27 11:32:52 -0700

    Update tagged_builder.rb helper for new 'super' syntax

--- app/helpers/tagged_builder.rb
+++ app/helpers/tagged_builder.rb
@@ -1,6 +1,6 @@
 class TaggedBuilder < ActionView::Helpers::FormBuilder
   HELPERS = field_helpers +
-            %w(date_select calendar_date_select collection_select select check_box) - 
+            %w(date_select calendar_date_select collection_select select check_box) -
             %w(hidden_field label fields_for)
   
   def self.created_tagged_field(method_name)
@@ -8,10 +8,13 @@ class TaggedBuilder < ActionView::Helpers::FormBuilder
       options = args.last.is_a?(Hash) ? args.pop : {}
       if options[:wraptags] == 'none'
         #super
-        super(name, *args)
+        #super(name, *args)
+        super(field, *(args << options))
       else
+        @template.content_tag(:th, (label(field, options[:label]) + ':') +
+          @template.content_tag(:td, super(field, *(args << options)), :colspan => options[:tdcolspan]))
         #@template.content_tag(:th, (label(field, options[:label]) + ':') +  @template.content_tag(:td, super, :colspan => options[:tdcolspan]))
-        @template.content_tag(:th, (label(field, options[:label]) + ':') +  @template.content_tag(:td, super(name, *args), :colspan => options[:tdcolspan]))
+        #@template.content_tag(:th, (label(field, options[:label]) + ':') +  @template.content_tag(:td, super(name, *args), :colspan => options[:tdcolspan]))
       end
     end
   end
