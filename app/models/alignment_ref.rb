7ed6aa60c484184327d0e35f1828c8392025daaf
suegrimes <sgrimes@stanford.edu>
2014-01-30 11:56:16 -0800

    Update model annotation

--- app/models/alignment_ref.rb
+++ app/models/alignment_ref.rb
@@ -13,19 +13,19 @@
 
 class AlignmentRef < ActiveRecord::Base
   DEFAULT_REF = 'HWG_37.1'
-  
+
   def self.default_id
     self.where(:alignment_key => DEFAULT_REF).pluck(:id).first
   end
-  
+
   def self.find_and_sort_all
     self.order("alignment_key").all
   end
-  
+
   def self.populate_dropdown
     self.find_and_sort_all
   end
-  
+
   def self.get_align_key(id=nil)
     return nil if id.nil?
     self.find(id).alignment_key
