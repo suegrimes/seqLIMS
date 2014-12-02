2442f323f32a4af9b825a5c865b589205e2b8fc7
Sue Grimes <sgrimes@stanford.edu>
2014-12-01 22:15:51 -0800

    Misc storage container query

--- config/routes.rb
+++ config/routes.rb
@@ -84,11 +84,9 @@ SeqLIMS::Application.routes.draw do
       get :auto_complete_for_barcode_key
     end
   end
-
   resources :sample_locs, :only => [:edit, :update]
 
   resources :sample_queries, :only => :index
-  resources :storage_queries, :only => :index
   #match 'sample_query' => 'sample_queries#index', :as => :sample_query
 
   resources :histologies do
